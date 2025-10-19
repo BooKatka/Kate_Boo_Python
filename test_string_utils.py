c85c436b01ddfac241d2d24c14c929919280a596
BooKatka <katebboo@yandex.ru>
2025-10-19 18:15:21 +0700

    Update test_string_utils.py

--- test_string_utils.py
+++ test_string_utils.py
@@ -51,7 +51,31 @@ def test_negative_trim(string_in, res_in):
       (" ", "H", False),
       ("123", "H", False)
        ])
+
+@pytest.mark.positive_test
+@pytest.mark.parametrize('string_in, symbol_in, res_in', [
+    ("Привет", "ив", "Прет"),
+    ("Balls", "s", "Ball"),
+    ("20 августа 1983", "1983", "20 августа"),
+    ("1920", "20", "19")
+    ])
+def test_positive_delete_symbol(string_in, symbol_in, res_in):
+    string = StringUtils()
+    res = string.delete_symbol(string_in, symbol_in)
+    assert res == res_in
+
+
+@pytest.mark.negative_test
+@pytest.mark.parametrize('string_in, symbol_in, res_in', [
+    ("Привет", " ", "Привет"),
+    ("Ball", "s", "Ball")
+    ])
+def test_negative_delete_symbol(string_in, symbol_in, res_in):
+    string = StringUtils()
+    res = string.delete_symbol(string_in, symbol_in)
+    assert res == res_in
 def test_negative_contains(string_in, symbol_in, res_in):
     string = StringUtils()
     res = string.contains(string_in, symbol_in)
+
     assert res == res_in
