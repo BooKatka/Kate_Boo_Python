abd50a36a670c6adcc3548050f56aa9b4a5d80e4
BooKatka <katebboo@yandex.ru>
2025-11-05 19:52:02 +0700

    Update test_02_calc.py

--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -5,24 +6,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome(
+def test_calculator():
+    driver = webdriver.Chrome(
     service=ChromeService(ChromeDriverManager().install()))
-driver.implicitly_wait(20)
+    driver.implicitly_wait(20)
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
-driver.get(
-    "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+    input_field = driver.find_element(By.ID, 'delay')
+    input_field.clear()
+    input_field.send_keys("45")
 
-input_field = driver.find_element(By.ID, 'delay')
-input_field.clear()
-input_field.send_keys("45")
+    driver.find_element(By.XPATH, "//span[text()='7']").click()
+    driver.find_element(By.XPATH, "//span[text()='+']").click()
+    driver.find_element(By.XPATH, "//span[text()='8']").click()
+    driver.find_element(By.XPATH, "//span[text()='=']").click()
 
-driver.find_element(By.XPATH, "//span[text()='7']").click()
-driver.find_element(By.XPATH, "//span[text()='+']").click()
-driver.find_element(By.XPATH, "//span[text()='8']").click()
-driver.find_element(By.XPATH, "//span[text()='=']").click()
-
-WebDriverWait(driver, 45).until(
-    EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"), "15")
-)
-
-driver.quit()
+    WebDriverWait(driver, 45).until(
+    EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"), "15"))
+    res = driver.find_element(By.CSS_SELECTOR, ".screen").text
+    assert res == "15"
+       
+    driver.quit()
